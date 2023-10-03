25231ef7133ca1007ed381f912fbededf8b3d9ed
marco.madalin.farcas <marco.madalin.farcas@est.fib.upc.edu>
2023-10-03 17:37:22 +0200

    downgraded react

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <main className="light">
-      <App />
-    </main>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
